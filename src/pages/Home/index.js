62c0bf6c5ef2c1c0160b3d6bdef511b6badd6273
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-30 17:39:08 -0300

    Projeto Filmes pt2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            filmes: []
+        };
+        this.loadFilmes = this.loadFilmes.bind(this);
+    }
+
+    componentDidMount(){
+        this.loadFilmes();
+    }
+
+    loadFilmes(){
+        //  https://sujeitoprogramador.com/r-api/?api=filmes
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json)=> {
+            this.setState({ filmes: json })
+            // console.log(json);
+        });
+    }
+
     render(){
         return(
-            <div>
-                Home
+            <div className="container">
+                
+                <div className="lista-filmes">
+                    {this.state.filmes.map( (filme) => {
+                        return(
+                            <article key={filme.id} className="filme">
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} alt="Capa" />
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Erro from './pages/Erro';
 
 const Routes = () => {
     return(
@@ -9,6 +10,7 @@ const Routes = () => {
             <Header />
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route path="*" component={Erro} />
             </Switch>
         </BrowserRouter>
     );
